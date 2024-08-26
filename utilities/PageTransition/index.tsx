45d958eaee5cd8a2467dbd737111f3d932c35937
Amirhossein Zandi <blackviper933@gmail.com>
2024-08-26 17:01:39 +0330

    "Updated styles in index.css and added new motion.div elements to PageTransition component in index.tsx"

--- utilities/PageTransition/index.tsx
+++ utilities/PageTransition/index.tsx
@@ -18,6 +18,18 @@ const PageTransitionComponent = ({ children }: { children: React.ReactNode }) =>
                     transition={{ duration: 0.5, ease: "easeOut" }}
                     exit={{ height: "140vh" }}
                 />
+
+
+                <motion.div
+                    className="text-white text-8xl fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[60]"
+                    initial={{ opacity: 1 }}
+                    animate={{ opacity: 0 }}
+                    transition={{ duration: 0.8, delay: 0.5, ease: "easeOut" }}
+                >
+                    <p>{pathName.substring(1)}</p>
+                </motion.div>
+
+
                 <motion.div
                     className="h-[140vh] w-screen fixed bg-black rounded-t-[100px] z-50 bottom-[-10%]"
                     initial={{ height: "140vh" }}
